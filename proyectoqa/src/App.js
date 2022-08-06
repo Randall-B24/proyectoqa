7f919f734894d322f2554e4bd5ca7de9fc796759
Randall-B24 <ranbermudez24@gmail.com>
2022-08-06 02:03:11 -0600

    nada

--- proyectoqa/src/App.js
+++ proyectoqa/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player';
+//import video from './assets/videos/video prueba 2.mp4'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ReactPlayer
+        url = {video}
+        controls
+        width = '70%'
+        height= '70%'
+        // loop (para que se vuelva a reproducir)
+
+        />
     </div>
   );
 }
